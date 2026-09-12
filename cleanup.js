ee13165feb18571eca31e2b836d1896e614c5409
Pixel-Arcade <louis.brunel1@monlycee.net>
2026-09-12 14:23:29 +0200

    Pixel Arcade 8.0 - load cloud config and clear old cache

--- cleanup.js
+++ cleanup.js
@@ -1,1 +1,3 @@
-(async()=>{try{if('serviceWorker' in navigator){const regs=await navigator.serviceWorker.getRegistrations();for(const r of regs){await r.unregister()}}if('caches' in window){const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)))}}catch(e){console.warn(e)}})();
+/* Désactive l'ancien service worker/cache et charge la configuration cloud avant games.js. */
+(async()=>{try{if('serviceWorker' in navigator){const regs=await navigator.serviceWorker.getRegistrations();for(const r of regs)await r.unregister()}if('caches' in window){const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)))}}catch(e){console.warn(e)}})();
+document.write('<script src="config.js"></script>');
