1fa311772c5c1ec3c3acb5731da7ba45e22ba182
Pixel-Arcade <louis.brunel1@monlycee.net>
2026-09-12 21:20:02 +0200

    Refresh PWA shell cache for install icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='pixel-arcade-pwa-v1';
-const APP_SHELL=['/','/index.html','/manifest.webmanifest','/style.css','/icon.svg'];
+const CACHE_NAME='pixel-arcade-pwa-v2';
+const APP_SHELL=['/','/index.html','/manifest.webmanifest','/style.css','/icon.svg','/icon-192.svg','/icon-512.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
 });
@@ -8,9 +8,13 @@ self.addEventListener('activate',event=>{
 });
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
   event.respondWith(fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+    if(response.ok){
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+    }
     return response;
   }).catch(()=>caches.match(event.request).then(r=>r||caches.match('/index.html'))));
 });
